db309d19284bc674505d1d67f83e5102b47f1aff
carlo <carlo_van_der_sloot@hotmail.com>
2024-11-11 15:01:40 +0100

    assignment 4 from class november 11th, extra help from Jonne for line 22:  if (Character.isLetter((char) z)), which helps to make this work for specifically text inputs, great for hangman!

---
+++ src/school/opdrachten/caesars_code.java
@@ -0,0 +1,40 @@
+package school.opdrachten;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class caesars_code {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word to encrypt using Ceasar's Coding system");
+        String input_word = input.nextLine();
+        char input_array[] = input_word.toCharArray();
+
+        System.out.println("Enter the value for n to use in Ceasar's Coding system (integer)");
+        int n = input.nextInt();
+
+        // String chckLower = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // String original_word = "";
+        String encrypted_word = "";
+
+        for (int i = 0; i < input_array.length; i++) {
+            int z = input_array[i] + n;
+            if (Character.isLetter((char) z)) {
+                char t = (char) (z);
+                if (Character.isLetter(t)) {
+                    encrypted_word = encrypted_word + t;
+                } else {
+                    encrypted_word = encrypted_word + input_array[i];
+                }
+            } else {
+                char t = (char) (z - 26);
+                if (Character.isLetter(t)) {
+                    encrypted_word = encrypted_word + t;
+                } else {
+                    encrypted_word = encrypted_word + input_array[i];
+                }
+            }
+        }
+        System.out.println("The original word is: \"" + input_word + "\" and the encrypted word, while n=" + n + " is: \"" + encrypted_word + "\".");
+    }
+}
