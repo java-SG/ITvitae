d2c01fa7f2e89447705907caa07981100cd9bf89
carlo <carlo_van_der_sloot@hotmail.com>
2024-11-18 15:54:58 +0100

    ignore the comments

--- src/playerguide/day11/BuyingInventory.java
+++ src/playerguide/day11/BuyingInventory.java
@@ -29,8 +29,89 @@ Build a program that will show the menu illustrated above.
 Ask the user to enter a number from the menu.
 Using the information above, use a switch (either type) to show the item’s cost.
 */
-public class BuyingInventory {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class BuyingInventory {
+
+    static int[] itemCount = {5, 1, 3, 1, 0, 1};
+    static String[] itemList = {"Rope", "Sword", "Bread", "Water", "Apple", "Mysterious Key"};
+    static float[] itemPrice = {1.50f, 15f, 3f, 1f, 1f, 100f};
+    static String selectedShopItem;
+    static float selectedShopPrice;
+    static int selectShopItemNumber;
+    static float discount;
+
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("0.00");
+        // Make method of items with itemName(itemAvailable) + itemPrice
+        // Print output to welcome to store, followed by scanner for input "Browse Shop", "Exit"
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hi there customer! What is your name?");
+        String name = input.nextLine();
+        if (name.equals("Carlo")) {
+            discount = 0.5f;
+        } else {
+            discount = 1.0f;
+        }
+        System.out.println("These are the items available currently in the store:");
+        for (int i = 0; i < itemList.length; i++) {
+            System.out.println("[" + (i + 1) + "] " + itemList[i] + " " + "-".repeat((15 - itemList[i].length())) + " available (" + itemCount[i] + ")");
+        }
+        System.out.println("Enter the [number] of the item above to select it");
+        selectShopItemNumber = input.nextInt();
+
+        System.out.println("The price of a " + selectionShopItem(selectShopItemNumber) + " is " + df.format(selectionShopPrice(selectShopItemNumber) * discount) + " gold.");
+
+        // int selectedShopItemNumber = input.nextInt();
+        // Print output with list of items prefixed with listNumber (-1 on array?)
+        // Make shop selection
+    }
+
+    static String selectionShopItem(int selectShopItemNumber) {
+        switch (selectShopItemNumber) {
+            case 1:
+                selectedShopItem = itemList[0];
+                break;
+            case 2:
+                selectedShopItem = itemList[1];
+                break;
+            case 3:
+                selectedShopItem = itemList[2];
+                break;
+            case 4:
+                selectedShopItem = itemList[3];
+                break;
+            case 5:
+                selectedShopItem = itemList[4];
+                break;
+            default:
+                selectedShopItem = "Error";
+        };
+        return selectedShopItem;
+    }
+
+    static float selectionShopPrice(int selectShopItemNumber) {
+        switch (selectShopItemNumber) {
+            case 1:
+                selectedShopPrice = itemPrice[0];
+                break;
+            case 2:
+                selectedShopPrice = itemPrice[1];
+                break;
+            case 3:
+                selectedShopPrice = itemPrice[2];
+                break;
+            case 4:
+                selectedShopPrice = itemPrice[3];
+                break;
+            case 5:
+                selectedShopPrice = itemPrice[4];
+                break;
+            default:
+                selectedShopPrice = 0f;
+        };
+        return selectedShopPrice;
     }
 }
